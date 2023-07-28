034a7be459b33420753f279c4acf0bdcc50b544f
Aleksandra Rączka <aleksandraraczkaa@gmail.com>
2023-07-28 12:21:10 +0200

    fixed Moves

--- js/script.js
+++ js/script.js
@@ -2,13 +2,14 @@ function printMessage(msg){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
-	let computerMove = `kamień`;
-	printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-
-	let playerMove='papier';
-	printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!');
 }
 
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
+}
+let computerMove = `kamień`; {
+	printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove='papier';
+	printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!');
 }

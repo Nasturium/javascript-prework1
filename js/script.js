4a7ba9eb301d1e6c1f8f108c89bd7c15fd922f1a
Aleksandra Rączka <aleksandraraczkaa@gmail.com>
2023-08-11 12:00:09 +0200

    new js script

--- js/script.js
+++ js/script.js
@@ -9,19 +9,15 @@ function clearMessages(){
 }
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-console.log('Wylosowana liczba to: ' + randomNumber);
-
-let computerMove = 'nieznany ruch';
+printMessage('Mój ruch to: ' + computerMove);
 
 if(randomNumber == 1){
-  computerMove = 'kamień';
-} else if (randomNumber == 2) {
-  computerMove('papier');
-} else if (randomNumber == 3) {
-  computerMove('nożyce');
-}
-
-printMessage('Mój ruch to: ' + computerMove);
+	computerMove = 'kamień';
+  } else if (randomNumber == 2) {
+	  computerMove = 'papier'; 
+  } else if (randomNumber == 3) {
+	  computerMove = 'nożyce';
+  }
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
@@ -31,10 +27,13 @@ let playerMove = 'nieznany ruch';
 
 if(playerInput == '1'){
   playerMove = 'kamień';
-} else if (playerInput == '2') {
-  playerMove('papier');
-} else if (playerInput == '3') {
-  playerMove('nożyce');
+}  else if (playerInput == '2') {
+	playerMove = 'papier';
+} else if (playerInput == '3'){
+	playerMove = 'nożyce';
 }
+if( computerMove == 'kamień' && playerMove == 'papier'){
+	printMessage('Ty wygrywasz!');
+  }
 
 printMessage('Twój ruch to: ' + playerMove);
